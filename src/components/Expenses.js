25a61c1b0e3dd45304281c05c850eaa161f358e1
HONGKAI123 <huanghongkai98@GMAIL.COM>
2023-04-02 22:39:46 -0700

    update dynamic list and condtional rendering

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -10,19 +10,53 @@ function Expenses(props) {
     console.log(years);
   };
   const [year, setYear] = useState("2022"); //这里不会显示default ，除非在filter里面设置default ， 使用props完成
+  const filteritem = props.item.filter((expense) => {
+    //把所有item拿出来filter ， return 和year 一样的object
+    return expense.date.getFullYear().toString() === year;
+  });
+
+  //conditonal rendering（way1）
+  // let filtercase = <p>nothing here </p>;
+  // if (filteritem.length > 0) {
+  //   filtercase = filteritem.map((data1) => (
+  //     <ExpenseItem
+  //       key={data1.id}
+  //       title1={data1.title}
+  //       date={data1.date}
+  //       amount={data1.amount}
+  //     />
+  //   ));
+  // }
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter onsaveyear={yearchangeHandeler} onSelected={year} />
+
         {/* onsaveyear={yearchangeHandeler} */}
-        {props.item.map((data1) => (
+        {filteritem.length === 0 && <p>noting here </p>}
+        {filteritem.length > 0 &&
+          filteritem.map((data1) => (
+            //way2
+            //props.item.map((data1) 变成filteritem.map((data1), 把data在map前先filter 一次
+            //data 是appjs 里面的array， map这个array里面的东西 然后使用props 设置在expenseitem里面
+            <ExpenseItem
+              key={data1.id}
+              title1={data1.title}
+              date={data1.date}
+              amount={data1.amount}
+            />
+          ))}
+        {/* {filteritem.map((data1) => (
+          //props.item.map((data1) 变成filteritem.map((data1), 把data在map前先filter 一次
           //data 是appjs 里面的array， map这个array里面的东西 然后使用props 设置在expenseitem里面
           <ExpenseItem
+            key={data1.id}
             title1={data1.title}
             date={data1.date}
             amount={data1.amount}
           />
-        ))}
+        ))} */}
+
         {/* {props.item.map((expenses) => (
           <ExpenseItem
             title1={expenses.title}
@@ -34,21 +68,6 @@ function Expenses(props) {
           title1={props.item[0].title}
           date={props.item[0].date}
           amount={props.item[0].amount}
-        ></ExpenseItem>
-        <ExpenseItem
-          title1={props.item[1].title}
-          date={props.item[1].date}
-          amount={props.item[1].amount}
-        ></ExpenseItem>
-        <ExpenseItem
-          title1={props.item[2].title}
-          date={props.item[2].date}
-          amount={props.item[2].amount}
-        ></ExpenseItem>
-        <ExpenseItem
-          title1={props.item[3].title}
-          date={props.item[3].date}
-          amount={props.item[3].amount}
         ></ExpenseItem> */}
       </Card>
     </div>
